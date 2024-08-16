3a154eb8cc5eedac1a1de1819ec8970efbe11c3e
tg0xff <tg0xff@proton.me>
2024-08-16 13:40:56 -0600

    Add test for Ship.isSunk()

--- src/classes.test.js
+++ src/classes.test.js
@@ -4,4 +4,8 @@ describe("Test Ship", () => {
     ship.hit();
     expect(ship.hits).toBe(4);
   });
+  test("Ship.isSunk()", () => {
+    const ship = new Ship(0);
+    expect(ship.isSunk()).toBe(true);
+  });
 });
