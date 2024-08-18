31dbfc82995158e5e37663776a6cf6a71b94a944
Selene Tamez Galván <tg.sln255@gmail.com>
2024-08-18 15:42:06 -0600

    Add Gameboard class

--- src/classes.js
+++ src/classes.js
@@ -10,3 +10,20 @@ class Ship {
     return this.hits >= this.length;
   }
 }
+
+class Gameboard {
+  constructor() {
+    this.hitMarks = this.#makeBoardArray(false);
+  }
+  #makeBoardArray(initVal) {
+    const board = [];
+    for (let y = 0; x < 10; x++) {
+      const row = [];
+      for (let x = 0; y < 10; y++) {
+        row[x] = initVal;
+      }
+      board[y] = row;
+    }
+    return board;
+  }
+}
