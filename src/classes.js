9dfa146788e99291e9e9324d00f86d6f9599cb25
tg0xff <tg0xff@proton.me>
2024-08-24 15:19:10 -0600

    Define populateBoard()

--- src/classes.js
+++ src/classes.js
@@ -2,6 +2,7 @@ class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
+    this.orientation = Math.random > 0.5 ? "horizontal" : "vertical";
   }
   hit() {
     this.hits++;
@@ -13,7 +14,10 @@ class Ship {
 
 class Gameboard {
   constructor() {
+    this.ships = [];
+    this.shipIndexBoard = this.#makeBoardArray(-1);
     this.hitMarks = this.#makeBoardArray(false);
+    this.populateBoard();
   }
   #makeBoardArray(initVal) {
     const board = [];
@@ -29,5 +33,16 @@ class Gameboard {
   receiveAttack(y, x) {
     if (this.hitMarks[y][x]) return;
     this.hitMarks[y][x] = true;
+  }
+  populateBoard() {
+    const shipLengths = [5, 4, 3, 3, 2];
+    for (let i = 0; i < shipLengths.length; i++) {
+      const ship = new Ship(shipLengths[i]);
+      do {
+        ship.x = Math.floor(Math.random() * 10);
+        ship.y = Math.floor(Math.random() * 10);
+      } while (!this.canBePlaced(ship));
+      this.placeShip(i, ship);
+    }
   }
 }
