86b576346ae8cc44f61b53357af61e6754737172
Selene Tamez Galván <tg.sln255@gmail.com>
2024-08-25 21:15:54 -0600

    Add properties for webpack's web server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
   plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
   module: {
     rules: [
