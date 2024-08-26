d5474b807426f2bfd1c11c545eb68379fd880eef
Everardo Tamez Galván <tg0xff@proton.me>
2024-08-25 21:10:02 -0600

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
