81bb25d5d157f8e04f8eb71c328337c79854f77e
Everardo Tamez Galván <tg0xff@proton.me>
2024-08-25 21:26:05 -0600

    Change webpack dev source map

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,8 @@ const common = require("./webpack.common.js");
 
 module.exports = {
   mode: "development",
-  devtool: "eval-source-map",
+  devtool: "inline-source-map",
   devServer: {
     static: "./dist",
-    watchFiles: ["./src/index.html"],
   },
 };
