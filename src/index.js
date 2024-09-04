ed3444e191b3a1223b2b5d52820c10459d7d4b48
Selene Tamez Galván <tg.sln255@gmail.com>
2024-09-03 22:10:29 -0600

    Define UI class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const ui = new (class UI {
+  constructor() {
+    this.playerBoardDiv = document.querySelector("#player-board");
+    this.opponentBoardDiv = document.querySelector("#opponent-board");
+    this.makeGrid(this.playerBoardDiv);
+    this.makeGrid(this.opponentBoardDiv);
+  }
+  makeGrid(boardDiv) {
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        const div = document.createElement("div");
+        div.setAttribute("data-coords", `${y},${x}`);
+        boardDiv.appendChild(div);
+      }
+    }
+  }
+})();
