387671a68bc874ed4e7422f5428696b4e3d477b3
Everardo Tamez Galván <tg0xff@proton.me>
2024-09-09 18:44:53 -0600

    Draw the player's ships

--- src/index.js
+++ src/index.js
@@ -9,14 +9,29 @@ class Game {
     this.opponentBoardDiv = document.querySelector("#opponent-board");
     this.makeGrid(this.playerBoardDiv);
     this.makeGrid(this.opponentBoardDiv);
+    this.drawShips(this.player, this.playerBoardDiv);
   }
   makeGrid(boardDiv) {
     for (let y = 0; y < 10; y++) {
       for (let x = 0; x < 10; x++) {
         const div = document.createElement("div");
+        div.className = "square";
+        div.style["grid-area"] = `${x + 1} / ${y + 1}`;
         div.setAttribute("data-coords", `${y},${x}`);
         boardDiv.appendChild(div);
       }
+    }
+  }
+  drawShips(player, boardDiv) {
+    for (const ship of player.gameboard.ships) {
+      const div = document.createElement("div");
+      div.className = "ship";
+      if (ship.orientation === "v") {
+        div.style["grid-area"] = `${ship.y + 1} / ${ship.x + 1} / span ${ship.length}`;
+      } else if (ship.orientation === "h") {
+        div.style["grid-area"] = `${ship.y + 1} / ${ship.x + 1} / span 1 / span ${ship.length}`;
+      }
+      boardDiv.appendChild(div);
     }
   }
 }
