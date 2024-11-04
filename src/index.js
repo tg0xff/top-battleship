9680d23f18c5cec02971b20b5d7906faa6194144
Everardo Tamez Galván <tg0xff@proton.me>
2024-11-03 21:50:45 -0600

    feat: implement cpu attacks

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ class Game {
     this.player = new Player(true, Ship, Gameboard);
     this.cpu = new Player(false, Ship, Gameboard);
     this.gameHasStarted = false;
+    this.isPlayerOnesTurn = true;
 
     this.playerBoardDiv = document.querySelector("#player-board");
-    this.opponentBoardDiv = document.querySelector("#opponent-board");
-    this.opponentBoardDiv.addEventListener("click", (e) => this.sendAttack(e));
+    this.cpuBoardDiv = document.querySelector("#opponent-board");
     this.randomBtn = document.querySelector("#random");
-    this.randomBtn.addEventListener("click", this.randomizeBoard.bind(this));
     this.startBtn = document.querySelector("#start");
 
+    this.cpuBoardDiv.addEventListener("click", (e) => this.sendAttack(e));
+    this.randomBtn.addEventListener("click", this.randomizeBoard.bind(this));
+    this.startBtn.addEventListener("click", () => (this.gameHasStarted = true));
+
     this.makeGrid(this.playerBoardDiv);
-    this.makeGrid(this.opponentBoardDiv);
+    this.makeGrid(this.cpuBoardDiv);
     this.drawShips(this.player, this.playerBoardDiv);
   }
   makeGrid(boardDiv) {
@@ -51,16 +54,37 @@ class Game {
     this.player.gameboard.placeShipsRandomly();
     this.drawShips(this.player, this.playerBoardDiv);
   }
+  drawSquares(player, gridElement) {
+    const squares = gridElement.querySelectorAll(".square");
+    squares.forEach((square) => {
+      const [y, x] = square.getAttribute("data-coords").split(",");
+      square.textContent = player.gameboard.getSquareState(y, x);
+    });
+  }
   sendAttack(e) {
-    if (!this.gameHasStarted) return;
-    if (!e.target.classList.contains("square")) return;
-    const [x, y] = e.target.getAttribute("data-coords").split(",");
+    if (
+      !this.gameHasStarted ||
+      !this.isPlayerOnesTurn ||
+      !e.target.classList.contains("square")
+    )
+      return;
+    const [y, x] = e.target.getAttribute("data-coords").split(",");
+    if (!this.cpu.gameboard.canBeAttacked(y, x)) return;
     this.cpu.gameboard.receiveAttack(y, x);
-    if (this.cpu.gameboard.shipIndexBoard[y][x] !== -1) {
-      e.target.textContent = "☠️";
-    } else {
-      e.target.textContent = "⚫️";
-    }
+    this.drawSquares(this.cpu, this.cpuBoardDiv);
+    this.isPlayerOnesTurn = false;
+    this.makeCPUAttack();
+  }
+  makeCPUAttack() {
+    let x;
+    let y;
+    do {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    } while (!this.player.gameboard.canBeAttacked(y, x));
+    this.player.gameboard.receiveAttack(y, x);
+    this.drawSquares(this.player, this.playerBoardDiv);
+    this.isPlayerOnesTurn = true;
   }
 }
 
